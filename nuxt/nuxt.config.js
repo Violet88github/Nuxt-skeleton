a2928d687698e53c85bb1cd228cf79e04ee556a0
thijsviolet88 <thijs@violet88.nl>
2023-06-05 16:36:27 +0200

    Fix: Added correct GraphQL configuration

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,16 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config"
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
-    '@nuxtjs/apollo',
+    '@nuxtjs/apollo'
+
   ],
   apollo: {
+    autoImports: true,
+    authType: 'Bearer',
+    authHeader: 'Authorization',
+    tokenStorage: 'cookie',
+    proxyCookies: true,
     clients: {
       default: {
         httpEndpoint: 'http://silverstripe:8888/dev/graphql'
       }
-    },
+    }
   },
+  build: {
+    hotMiddleware: {
+      client: {
+        overlay: false
+      }
+    }
+  },
+  colorMode: {
+    classSuffix: ''
+  }
 })

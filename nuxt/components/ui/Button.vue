814c096d9c95d97bdf04632d81d921a3be830ec6
thijsviolet88 <thijs@violet88.nl>
2023-06-06 16:12:47 +0200

    fix: Fixed linting problem and added standard ui components

---
+++ nuxt/components/ui/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <component 
+    @click="$emit('click')"
+    class="flex items-center justify-center btn" 
+    :is="is" 
+    :class="{
+      'btn-primary': type === 'primary'
+    }
+    "
+  >
+    <UiIcon
+      v-if="icon"
+      size="medium"
+      name="name"
+    />
+    {{ label ?? '' }}
+  </component>
+</template>
+  
+<script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+  
+const props = defineProps<{
+  label?: string;
+  to?: URL;
+  blank?: boolean;
+  icon?: string;
+  type: string;
+}>();
+
+
+const is = computed(() => {
+  if (props.to) {
+    return "NuxtLink";
+  }
+
+  return "button";
+});
+</script>

bc1d01ee5152b35a9f54defa94d0445d3313f175
thijsviolet88 <thijs@violet88.nl>
2023-12-18 13:11:49 +0100

    Update parseSilverStripeNames.ts

--- composables/parseSilverStripeNames.ts
+++ composables/parseSilverStripeNames.ts
@@ -1,13 +1,5 @@
 export const parseSilverStripeNames = (inputString: string): string  => {
-    const slashes = 2;
-    const parts = inputString.split('\\');
+  const parts = inputString.split('\\');
   
-    if (parts.length >= slashes) {
-      const lastTwoSlashes = parts.slice(-slashes).join('\\');
-  
-      const lastPart = parts[parts.length - 1];
-      return lastPart;
-    }
-  
-    return '';
+  return parts[parts.length - 1];
 }
